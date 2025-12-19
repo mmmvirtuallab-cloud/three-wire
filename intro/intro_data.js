29cfdcd3e7a52523fb11e43f2bb2d47bda041721
Your Name <your.email@example.com>
2025-12-20 00:47:49 +0530

    updted for local build

---
+++ intro/intro_data.js
@@ -0,0 +1,29 @@
+// js/intro_data.js
+
+export const INTRO_CONTENT = {
+    // Top Blue Bar Text
+    headerTitle: "Three wire method ",
+    
+    // Button Text & Link
+    buttonLabel: "Go to Experiment",
+    buttonLink: "lab.html",
+
+    // Main Page Content
+    mainHeading: "Three Wire Method for Measurement of Diameter",
+    
+    // Aim Section
+    aimTitle: "Aim",
+    aimContent: "To study the working principle of a profile projector and to measure the dimensions of given components such as screw threads, gears, and other complex profiles accurately.",
+
+    // Acknowledgement Section
+    ackTitle: "Acknowledgement",
+    ackContent: "The Department of Production Technology appreciates the following students for their support in the development of Metrology and measurements virtual laboratory experiment.",
+    
+    // Student List
+    students: [
+        "A. M. ISMAIL - 2023507030",
+        "V. B. JAYARAM - 2023507040",
+        "M. RAM BHARATH - 2023507039",
+        "R. MONISH KUMAR - 2023507038"
+    ]
+};
